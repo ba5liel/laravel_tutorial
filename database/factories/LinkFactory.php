036fcea5ce61c2582877fdabda4f6c1d40c025e9
Selamu <baslielselamu2018@gmail.com>
2020-06-28 13:16:52 +0300

    First laravel

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Link;
+use Faker\Generator as Faker;
+
+$factory->define(Link::class, function (Faker $faker) {
+    return [
+        'title'=>$faker->sentence(),
+        'url'=>$faker->url,
+        'description'=>$faker->paragraph()
+    ];
+});
